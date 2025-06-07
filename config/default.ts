03881ad88376de3d55832e8ab8ee664df0456ee8
NERD777 <nugrohoandi2005@gmail.com>
2025-06-08 05:05:26 +0700

    Update default.ts

---
+++ config/default.ts
@@ -0,0 +1,9 @@
+export default {
+  port: 6969,
+  dbUri: 'mongodb://127.0.0.1:27017/rest-api',
+  saltWorkFactor: 10,
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
+  publicKey: `ADD PUBLIC KEY HERE`,
+  privateKey: `ADD PRIVATE KEY HERE`,
+};
